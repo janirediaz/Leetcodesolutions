786d4d51f0a0fa1c048428d4587f718044f2d783
Janire <janire.diaz@gmail.com>
2015-11-20 10:02:33 +0100

    Two new exercises: Rectangle Area and Valid Parentheses

---
+++ easy/Rectangle Area.java
@@ -0,0 +1,10 @@
+public class Solution {
+    public int computeArea(int A, int B, int C, int D, int E, int F, int G, int H) {
+        int sum = (C-A)*(D-B) + (G-E)*(H-F);
+        if(C<=E||A>=G||B>=H||D<=F){
+            return sum;
+        }
+        int total = (Math.min(C,G)-Math.max(A,E))*(Math.min(D,H)-Math.max(B,F));
+        return sum-total;
+    }
+}
